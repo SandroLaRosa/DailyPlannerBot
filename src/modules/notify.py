d554c8fd543a60894032942772033baf4f0ae2be
Alpha-Lex <sandro.larosa1998@gmail.com>
2026-03-25 09:20:07 +0100

    fix: now the notify module can correctly interact with the context of the bot

--- src/modules/notify.py
+++ src/modules/notify.py
@@ -3,7 +3,7 @@ from classes.event_manager import EventManager
 
 async def notify_event(context:ContextTypes.DEFAULT_TYPE)->None:
     event_id:str=context.job.data
-    em:EventManager=context.job.data["event_manager"]
+    em:EventManager=context.bot_data["event_manager"]
     chat_id:int=context.bot_data["chat_id"]
 
     event=em.events.get(event_id)
